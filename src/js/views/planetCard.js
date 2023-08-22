6c0f742b8f25727e0bcb1dd699b73d465fb63014
labs404 <136141267+labs404@users.noreply.github.com>
2023-08-22 00:49:22 +0000

    added proptypes to planetCard.js

--- src/js/views/planetCard.js
+++ src/js/views/planetCard.js
@@ -20,6 +20,19 @@ const PlanetCard = (props) => {
                     <h1>{props.name}</h1>
                     <p>population: {props.population}</p>
                     <p>terrain: {props.terrain}</p>
+
+                    <p>name : {props.name}</p>
+                    <p>rotation_period : {props.rotation_period}</p>
+                    <p>orbital_period : {props.orbital_period}</p>
+                    <p>diameter : {props.diameter}</p>
+                    <p>climate : {props.climate}</p>
+                    <p>gravity : {props.gravity}</p>
+                    <p>terrain : {props.terrain}</p>
+                    <p>surface_water : {props.surface_water}</p>
+                    <p>population : {props.population}</p>
+                    <p>residents : {props.residents}</p>
+                    <p>films : {props.films}</p>
+                    <p>url : {props.url}</p>
                     <div className="row">
                         <div className="col text-start">
                             <a href={props.url} className="btn btn-primary">Learn More!</a>
@@ -35,7 +48,18 @@ const PlanetCard = (props) => {
 }
 
 PlanetCard.propTypes = {
-    name: propTypes.string
+    name: propTypes.string,
+    rotation_period: propTypes.number,
+    orbital_period: propTypes.number,
+    diameter: propTypes.number,
+    climate: propTypes.string,
+    gravity: propTypes.string,
+    terrain: propTypes.string,
+    surface_water: propTypes.number,
+    population: propTypes.number,
+    residents: propTypes.string,
+    films: propTypes.string,
+    url: propTypes.string
 }
 
 export default PlanetCard;
